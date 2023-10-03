931c2dc4212d5cfaac00b11f722f3778ab417740
Dileep <dileepvunnam@gmail.com>
2023-10-03 18:40:44 +0100

    project setup

---
+++ Utils/firebase.js
@@ -0,0 +1,18 @@
+import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import Constants from 'expo-constants';
+
+
+const firebaseConfig = {
+    apiKey: Constants.expoConfig?.extra?.firebaseApiKey,
+    authDomain: Constants.expoConfig?.extra?.firebaseAuthDomain,
+    projectId: Constants.expoConfig?.extra?.firebaseProjectId,
+    storageBucket: Constants.expoConfig?.extra?.firebaseStorageBucket,
+    messagingSenderId: Constants.expoConfig?.extra?.firebaseMessagingSenderId,
+    appId: Constants.expoConfig?.extra?.firebaseAppId,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+export default auth;
